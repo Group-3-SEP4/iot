b8f75290eaad1594f310569e9339ae5dc2b08e91
273962 <48473715+273962@users.noreply.github.com>
2020-12-10 17:33:42 +0100

    refact(ht.h)

--- IoT_sleep/IoT_sleep/Includes/ht_service.h
+++ IoT_sleep/IoT_sleep/Includes/ht_service.h
@@ -6,10 +6,10 @@
 
 typedef struct ht_measurement* ht_t;
 
-ht_t ht_create(EventGroupHandle_t event_group_data_collect, EventGroupHandle_t event_group_data_ready);
-int16_t ht_get_temperature(ht_t sensor);
-uint16_t ht_get_humidity(ht_t sensor);
-void ht_measure(ht_t sensor);
+ht_t ht_service_create(EventGroupHandle_t event_group_data_collect, EventGroupHandle_t event_group_data_ready);
+int16_t ht_service_get_temperature(ht_t sensor);
+uint16_t ht_service_get_humidity(ht_t sensor);
+void ht_service_measure(ht_t sensor);
 
 
 
