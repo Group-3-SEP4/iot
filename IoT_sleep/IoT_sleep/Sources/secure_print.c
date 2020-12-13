deff71d9c0c5b9701f37312ff300021637a88a5a
Stygg <48440436+VIA-Jesper@users.noreply.github.com>
2020-12-13 18:00:58 +0100

    fix(secure_print): tried to fix a memory issue

--- IoT_sleep/IoT_sleep/Sources/secure_print.c
+++ IoT_sleep/IoT_sleep/Sources/secure_print.c
@@ -20,7 +20,7 @@ void s_print_create(SemaphoreHandle_t mutex) {
 void s_print(char* level, char* tag, char* msg, ...) {
 	if (NULL != mutex_print && (DEF_PRINT_TO_TERMINAL == true || strcmp("INFO", level) != 0)){ 
 		if (xSemaphoreTake(mutex_print, DEF_WAIT_DEFAULT) == pdTRUE) { 
-			static char buffer[128];
+			static char buffer[128]; // automatically cuts the message if bigger than 128 chars.
 			va_list args;
 			va_start(args, msg);
 			vsprintf(buffer, msg, args);
